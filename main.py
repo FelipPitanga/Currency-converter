ecf0a595aa324e7b2e0cc838d22e8c25b991799f
FelipPitanga <pitangafelipe2016@gmail.com>
2022-07-08 08:08:56 -0300

    new img, ajustes de frames

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-from tkinter import _Compound, _Relief, Tk, font, ttk
+from tkinter import Tk, ttk
 from tkinter import *
-from turtle import width 
 
 #library import 
 from PIL import Image, ImageTk, ImageOps, ImageDraw
@@ -30,16 +29,16 @@ frame2 = Frame(janela, width=300, height=260, padx= 0, bg=cor0, relief="flat")
 frame2.grid(row=1, column=0, sticky=NSEW)
 
 #settings frame1
-ico1 = Image.open('imgs/img1.jgp')
-ico1 = ico1.resize((40, 40), Image.ANTIALIAS)
+ico1 = Image.open("imgs/dinheiro.png")
+ico1 = ico1.resize((45,45), Image.ANTIALIAS)
 ico1 = ImageTk.PhotoImage(ico1)
 
-nm = Label(frame1, image=ico1, compound=LEFT, text="Currency-converter", height=30, padx=30, pady=13, relief="raised", anchor=CENTER, bg=cor2, fg=cor0)
-nm.Place(x=0, y=0)
+nmpp = Label(frame1, image=ico1, compound=LEFT, text="Currency-converter", height=30, padx=20, pady=13, relief="raised", anchor=CENTER, font='Arial 16 bold', bg=cor1, fg=cor0)
+nmpp.place(x=0, y=0)
 
 #settings frame2
-nmresult = Label(frame2, compound=LEFT, width=16, height=2, relief="solid", anchor=CENTER, bg=cor0, fg=cor1)
-nmresult.Place(x=50, y=10)
+nmresult = Label(frame2, width=16, height=2, relief="solid", anchor=CENTER, font='Ivy 15 bold', bg=cor0, fg=cor1)
+nmresult.place(x=50, y=10)
 
 
 janela.mainloop()
